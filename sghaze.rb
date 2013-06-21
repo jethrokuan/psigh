e0215df1d682ba299c300248b71344b629bb200e
Jethro Kuan <jethrokuan95@gmail.com>
2013-06-21 13:57:21 +0800

    added shit algo

--- sghaze.rb
+++ sghaze.rb
@@ -1,4 +1,36 @@
 class SgHaze < Sinatra::Base
+  def current_time
+    hour = Time.now.hour
+    if hour < 12
+      if hour == 0
+        current_time = "12AM"
+      else
+        current_time = "#{hour}AM"
+      end
+    else
+      current_time="#{hour-12}PM"
+    end
+  end
+  def hourly_data
+    original_data = data().to_a
+    new_data = Hash.new
+    original_data.each_with_index do |a,index|
+      if index == 0
+        new_data["12AM"] = original_data[0][1]
+      elsif index == 1
+        new_data["1AM"] = original_data[1][1]
+      end
+      if index > 1
+        if original_data[index][1] == "-"
+          new_data[original_data[index][0]] = "-"
+        else
+          new_data[original_data[index][0]] = (3*original_data[index][1].to_i-original_data[index-1][1].to_i-original_data[index-2][1].to_i).to_s
+        end
+      end
+    end
+    return new_data
+  end
+
   def data
     doc = Nokogiri::HTML(open('http://app2.nea.gov.sg/anti-pollution-radiation-protection/air-pollution/psi/psi-and-pm2-5-readings'))
     node = doc.at_css('div.c1 >div >table')
@@ -18,12 +50,23 @@ class SgHaze < Sinatra::Base
     end
     return hash_data
   end
+  
+  def current_reading
+    current = data()
+    return current[current_time()]
+  end
   get '/' do
-    @hash_data = data()
+    @current_reading = current_reading()
+    @hourly_data = hourly_data()
+    @three_hour_data = data()
     erb :index
   end
 
   get '/json' do
     return data().to_json
+  end
+
+  get '/hourly' do
+    return hourly_data().to_json
   end
 end
